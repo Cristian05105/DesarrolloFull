077f3ee30e8b6378171d1342c7bb4409f906f7ba
cristian <cvillalpandoa@gmail.com>
2026-01-15 22:43:42 -0600

    archivos actualizados para entrega

---
+++ act1/java.js
@@ -0,0 +1,10 @@
+alert( "aviso" );
+
+function funcion (){
+const x= document.getElementById ("seccion"); 
+const y= document.getElementById ("seccion"); 
+
+
+x.style.backgroundColor="red";
+y.style.font="Times New Roman";
+}
